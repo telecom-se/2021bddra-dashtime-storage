dd3ef2c7f86d3193e7ffc1ba7ba2627be834e177
PBre42 <49883776+PBre42@users.noreply.github.com>
2022-01-06 11:47:55 +0100

    Reader

--- src/main/java/telecom/Main.java
+++ src/main/java/telecom/Main.java
@@ -1,5 +1,6 @@
 package telecom;
 
+<<<<<<< Updated upstream
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -7,17 +8,26 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+=======
+import java.io.IOException;
+>>>>>>> Stashed changes
 import java.util.Arrays;
 import java.util.Collection;
-
+import LoadDatabase.DataGenerator;
 import Types.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
+<<<<<<< Updated upstream
 		 Collections collec = new Collections();
 		 /*
+=======
+		/*
+		Collections collec = new Collections();
+
+>>>>>>> Stashed changes
 		Collection<Data> toto = Arrays.asList(new Data(new Timestamp((long) 1), 40.0), new Data(new Timestamp((long) 2), 15.0), new Data(new Timestamp((long) 3), 2024.0));
 		collec.addSerie("Toto", new Series(toto));
 		
@@ -27,6 +37,7 @@ public class Main {
 		//System.out.println(collec);
 
 		
+<<<<<<< Updated upstream
 		
 		
 		//collec.SaveData("tmp/serialize_collec.ser");
@@ -34,5 +45,19 @@ public class Main {
 	
 
 		 // fermeture du flux dans le bloc finally*/
+=======
+		System.out.println(collec);
+		*/
+		
+		Collections collec2 = new Collections();
+		
+		//Génère un fichier de 500 lignes
+		DataGenerator dg = new DataGenerator(500);
+		
+		//Remplit collec2
+		dg.ReadData(collec2,"500");
+		
+		
+>>>>>>> Stashed changes
 	}
 }
